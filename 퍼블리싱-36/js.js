13d6fc53475bf7d07de1d403a21104f774bc2780
AhnNagwang <ahnnagwang@naver.com>
2023-06-03 21:44:46 +0900

    퍼플리싱-36
    공부용

---
+++ 퍼블리싱-36/js.js
@@ -0,0 +1,10 @@
+$('.title').click(function() {
+  $(this).addClass('active')
+  $(this).siblings('.title').removeClass('active')
+  $(this).siblings('.desc').stop().slideUp()
+  $(this).next().stop().slideDown()
+
+   let dataImage = $(this).attr('data-image')
+   $('.image img').attr('src', dataImage)
+})
+
