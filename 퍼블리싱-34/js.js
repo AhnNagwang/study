acd99f18811ce83d82e0a92d7b7ac1e643d068db
AhnNagwang <ahnnagwang@naver.com>
2023-06-03 17:14:37 +0900

    퍼블리싱-34
    공부용

---
+++ 퍼블리싱-34/js.js
@@ -0,0 +1,12 @@
+$('.navi li').mouseenter(function() {
+  let changeImg = $(this).attr('data-image')
+  $('.photo').css({
+    'background-image': 'url('+changeImg+')'
+  })
+})
+
+$('.navi li').mouseleave(function() {
+  $('.photo').css({
+    'background-image':''
+  })
+})
