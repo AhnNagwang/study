11617083e15d6b631d936682b411d81444827c6b
AhnNagwang <ahnnagwang@naver.com>
2023-06-04 15:34:16 +0900

    퍼블리싱-36
    공부용

--- 퍼블리싱-36/js.js
+++ 퍼블리싱-36/js.js
@@ -1,10 +1,12 @@
+
+
 $('.title').click(function() {
   $(this).addClass('active')
   $(this).siblings('.title').removeClass('active')
+
   $(this).siblings('.desc').stop().slideUp()
   $(this).next().stop().slideDown()
 
-   let dataImage = $(this).attr('data-image')
-   $('.image img').attr('src', dataImage)
+  let dataImage = $(this).attr('data-image')
+  $('.image img').attr('src',dataImage)
 })
-
