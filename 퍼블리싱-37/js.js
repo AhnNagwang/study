3bd909a54045de6218618e2476455df91ce129cb
AhnNagwang <ahnnagwang@naver.com>
2023-06-06 15:56:50 +0900

    퍼블리싱-37
    공부용

--- 퍼블리싱-37/js.js
+++ 퍼블리싱-37/js.js
@@ -2,7 +2,5 @@ $('.trigger').click(function() {
   $(this).toggleClass('active')
   $('.modal-gnb').fadeToggle()
 })
-
-//audio
 let bgm = document.querySelector('#myAudio')
-bgm.volume = 0.2
+bgm.volume = 0.2;
